8a5eacf53e4d1af11b1a41472b42506dead1d309
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-03-08 17:19:05 -0400

    simple header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TorrentsTableComponent } from './torrents-table/torrents-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TorrentsTableComponent
   ],
   imports: [
     BrowserModule,
