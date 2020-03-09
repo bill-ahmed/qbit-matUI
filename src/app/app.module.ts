136b73577f79ed5dbc1c051a80a55db2a3812f4b
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-03-08 20:25:24 -0400

    login component, home component, and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TorrentsTableComponent } from './torrents-table/torrents-table.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TorrentsTableComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: "", component: LoginComponent},
+      { path: "home", component: AppComponent}
+    ]),
     HttpClientModule,
   ],
   providers: [CookieService],
