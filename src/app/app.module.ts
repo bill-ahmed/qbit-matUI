0fb42c81f674d7a8708640d85d207d65c83ff4cd
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-03-09 21:00:58 -0400

    fix routing & styles for login/home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
+// Material theme components
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TorrentsTableComponent } from './torrents-table/torrents-table.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'home', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
@@ -20,11 +28,9 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot([
-      { path: "", component: LoginComponent},
-      { path: "home", component: AppComponent}
-    ]),
+    RouterModule.forRoot(appRoutes),
     HttpClientModule,
+    MatFormFieldModule
   ],
   providers: [CookieService],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { GetCookieInfo } from '../../utils/ClientInfo';
+import { HttpClient } from '@angular/common/http';
+import { HttpConfigType } from '../../utils/Interfaces';
+import * as http_endpoints from '../../assets/http_config.json';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  private http_endpoints: any;
+  private cookieValueSID: string
 
-  constructor() { }
+  constructor(private router: Router, private cookieService: CookieService, private http: HttpClient) { this.http_endpoints = http_endpoints }
 
   ngOnInit(): void {
+  }
+
+  /**Login a user. Will set a cookie with appropriate credentials */
+  login(): void {
+    
+    // Grab username/password info
+    let usernameField: any = document.getElementById('username');
+    let passwordField: any = document.getElementById('password');
+
+    let username = (usernameField.value as string).trim()
+    let password = (passwordField.value as string)
+
+    if (username.length === 0) { alert("Username can't be empty!"); return; };
+    if (password.length === 0) { alert("Password can't be empty!"); return; };
+
+    // POST request to get cookie
+    let root = this.http_endpoints.default.endpoints.root;
+    let endpoint = this.http_endpoints.default.endpoints.login;
+
+    let body = new FormData();
+    body.append('username', username);
+    body.append('password', password);
+
+    let url = root + endpoint;
+
+    let xhr = new XMLHttpRequest();
+
+    // xhr.open('POST', url, true);
+    // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=utf-8');
+    // xhr.addEventListener('readystatechange', function() {
+    //     if (xhr.readyState === 4) { // DONE state
+    //         if (xhr.status === 200){
+    //           alert("done");
+    //         }
+    //         else{
+    //           alert("else");
+    //         }
+                
+    //     }
+    // });
+
+    // const queryString = "username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password);
+    // xhr.send(queryString);
+    console.log(url)
+    this.http.post(url, body).subscribe((data: any) => console.log(data));
   }
 
 }
