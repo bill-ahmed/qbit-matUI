aab61e64bfa1552eeeb840fd03ffe389c8b6e65f
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-03-22 17:56:15 -0400

    styles and callbacks for bulk update

--- src/app/torrents-table/bulk-update-torrents/bulk-update-torrents.component.ts
+++ src/app/torrents-table/bulk-update-torrents/bulk-update-torrents.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
+import { Torrent } from 'src/utils/Interfaces';
 
 @Component({
   selector: 'app-bulk-update-torrents',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BulkUpdateTorrentsComponent implements OnInit {
 
-  constructor() { }
+  public numTorrentsSelected: number = 0;
+
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public torData: Torrent[], private snackbarREF: MatSnackBarRef<BulkUpdateTorrentsComponent>) { }
 
   ngOnInit(): void {
+    this.numTorrentsSelected = this.torData.length;
+  }
+
+  /** Get appropriate message to display in snackbar */
+  public getSnackbarMessage(): string {
+    return this.numTorrentsSelected === 1 ? 
+        `1 torrent selected.` : `${this.numTorrentsSelected} torrents selected.`;
+  }
+
+  /** Programtically close the snackbar.
+   * @param withAction Whether the snackbar was dismissed with an action or not.
+   */
+  handleSnackbarClose(withAction: boolean): void {
+    withAction ? this.snackbarREF.dismissWithAction() : this.snackbarREF.dismiss();
+  }
+
+  /** Callback for when user chooses to cancel bulk deleting torrents */
+  handleCancelAction(): void {
+    this.handleSnackbarClose(false);
+  }
+
+  /** Callback for when user chooses to delete torrents */
+  handleDeleteAction(): void {
+    this.handleSnackbarClose(true);
   }
 
 }
