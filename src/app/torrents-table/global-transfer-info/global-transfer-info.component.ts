22231080983a10f8cc4cb3db1575aa1ee19e2d74
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-03-23 16:53:20 -0400

    logic for getting global state info & update styles

--- src/app/torrents-table/global-transfer-info/global-transfer-info.component.ts
+++ src/app/torrents-table/global-transfer-info/global-transfer-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TorrentDataStoreService } from 'src/app/services/torrent-management/torrent-data-store.service';
+import { GlobalTransferInfo, MainData } from 'src/utils/Interfaces';
 
 @Component({
   selector: 'app-global-transfer-info',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalTransferInfoComponent implements OnInit {
 
-  constructor() { }
+  public data: GlobalTransferInfo = null;
+
+  constructor(private data_store: TorrentDataStoreService) { }
 
   ngOnInit(): void {
+    
+    // Subscribe to any changes with data store
+    this.data_store.GetTorrentDataSubscription().subscribe((res: MainData) => {
+      if(res) {
+        this.handleDataChange(res.server_state);
+      }
+    })
+  }
+
+  handleDataChange(newData: GlobalTransferInfo): void {
+    console.log(newData);
+    this.data = newData;
+  }
+
+  isLoading(): boolean {
+    return !this.data;
   }
 
 }
