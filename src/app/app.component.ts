5db4436a2b00e2f8a307514d609009cb1ee0d91b
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-04-15 22:03:05 -0400

    dark theme at root level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './services/theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'qbit-WebUI';
+  public isDarkTheme: Observable<boolean>;
+
+  constructor(private theme: ThemeService) { }
+
+  ngOnInit() { this.isDarkTheme = this.theme.getThemeSubscription(); }
 }
