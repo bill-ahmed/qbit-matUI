b6a2ca7f00d0a18d2345bcfb04d8a8214dfaa427
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-05-28 15:28:42 -0400

    wrap up UI for downloads settings

--- src/app/modals/settings/download-settings/download-settings.component.ts
+++ src/app/modals/settings/download-settings/download-settings.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplicationConfigService } from 'src/app/services/app/application-config.service';
+import { DownloadSettings } from 'src/utils/Interfaces';
 
 @Component({
   selector: 'app-download-settings',
   templateUrl: './download-settings.component.html',
-  styleUrls: ['./download-settings.component.css']
+  styleUrls: ['./download-settings.component.scss']
 })
 export class DownloadSettingsComponent implements OnInit {
 
-  constructor() { }
+  downloadSettings: DownloadSettings;
+  downloadOptions: { };
 
-  ngOnInit(): void {
+  constructor(private appConfig: ApplicationConfigService) {
+    this.appConfig.getUserPreferences()
+    .then(pref => {
+      this.downloadSettings = {
+        max_active_downloads: pref.max_active_downloads,
+        max_active_torrents: pref.max_active_torrents,
+        max_active_uploads: pref.max_active_uploads,
+        save_path: pref.save_path,
+        scan_dirs: pref.scan_dirs,
+        temp_path: pref.temp_path
+      }
+    });
   }
+
+  ngOnInit(): void { }
+
 
 }
