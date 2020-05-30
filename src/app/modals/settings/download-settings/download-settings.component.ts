53cc6fbd1a71fc4ebbb42c6bb62bbf74e10d3f91
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-05-30 17:24:10 -0400

    validations for download and webui settings

--- src/app/modals/settings/download-settings/download-settings.component.ts
+++ src/app/modals/settings/download-settings/download-settings.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApplicationConfigService } from 'src/app/services/app/application-config.service';
 import { DownloadSettings } from 'src/utils/Interfaces';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-download-settings',
@@ -12,10 +12,11 @@ export class DownloadSettingsComponent implements OnInit {
 
   downloadSettings: DownloadSettings = { queueing_enabled: false, max_active_downloads: 0, max_active_torrents: 0, max_active_uploads: 0, save_path: "", scan_dirs: [], temp_path: "" };
 
-  common_validations = [];
+  common_validations = [Validators.min(0)];
   form_controls = {
     max_active_downloads: new FormControl(this.downloadSettings.max_active_downloads, [...this.common_validations]),
-    max_active_torrents: new FormControl(this.downloadSettings.max_active_torrents, [...this.common_validations])
+    max_active_torrents: new FormControl(this.downloadSettings.max_active_torrents, [...this.common_validations]),
+    max_active_uploads: new FormControl(this.downloadSettings.max_active_uploads, [...this.common_validations])
   }
 
   constructor(private appConfig: ApplicationConfigService) {
