c94c396f3844056b01a687c8e4dfacd1cccbd203
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-05-30 16:52:05 -0400

    styles - refactor all common styles into main stylesheet settings - add speed settings UI - interface for managing settings

--- src/app/modals/settings/speed-settings/speed-settings.component.ts
+++ src/app/modals/settings/speed-settings/speed-settings.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { SpeedSettings } from 'src/utils/Interfaces';
+import { ApplicationConfigService } from 'src/app/services/app/application-config.service';
 
 @Component({
   selector: 'app-speed-settings',
   templateUrl: './speed-settings.component.html',
-  styleUrls: ['./speed-settings.component.css']
+  styleUrls: ['./speed-settings.component.scss']
 })
 export class SpeedSettingsComponent implements OnInit {
 
-  constructor() { }
+  speed_settings: SpeedSettings = { up_limit: -1, dl_limit: -1 };
+
+  constructor(private appConfig: ApplicationConfigService) {
+    this.appConfig.getUserPreferences()
+    .then(pref => {
+      this.speed_settings = {
+        up_limit: pref.up_limit,
+        dl_limit: pref.dl_limit
+      }
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  /** Use ViewChild on this component
+   * in order to grab this data.
+   */
+  getSettings(): SpeedSettings {
+    return this.speed_settings;
   }
 
 }
