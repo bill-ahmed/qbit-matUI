eb7e1c1d6c2840d8cc7dca1606a3e0f63e751833
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-10-19 22:38:32 -0400

    fix css styles & some cleanup

--- src/utils/mappings.ts
+++
@@ -1,9 +0,0 @@
-export const TorrentColumnNameToFieldMapping = {
-  Name: 'name',
-  Size: 'size',
-  Progress: 'progress',
-  Status: 'state',
-  'Down Speed': 'dlspeed',
-  'Up Speed': 'upspeed',
-  'Completed On': 'completion_on'
-}
