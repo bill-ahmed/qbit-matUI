a46f23bce54a54dc082d51a4b88e8493a9968961
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-10-20 22:25:14 -0400

    helper method to deep merge two objects

--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -22,3 +22,35 @@ export function GetDefaultSaveLocation(): string {
 
   return save_location || "";
 }
+
+
+/**
+ * Simple object check.
+ */
+function isObject(item: any): boolean {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+/**
+ * Deep merge two objects.
+ *
+ * @description This was taken from a stackoverflow article :P
+ * @see https://stackoverflow.com/a/34749873
+ */
+export function MergeDeep(target: any, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        MergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return MergeDeep(target, ...sources);
+}
