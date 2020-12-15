ef18fe1831cba9165743f559c64ff9aa1217ff3f
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-12-15 18:43:46 -0500

    theme service for login page and more modules

--- projects/login/src/app/app.module.ts
+++ projects/login/src/app/app.module.ts
@@ -12,8 +12,10 @@ import { HttpClientModule } from '@angular/common/http';
 // Material Components
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 
 var appRoutes: Routes;
@@ -37,7 +39,9 @@ appRoutes = [
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
