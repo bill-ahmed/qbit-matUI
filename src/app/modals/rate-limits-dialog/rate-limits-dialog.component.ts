86fddbbb4d5b9bd35e8d70de4e020eee33418e1f
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2021-01-09 21:34:00 -0500

    set limit via UI

--- src/app/modals/rate-limits-dialog/rate-limits-dialog.component.ts
+++ src/app/modals/rate-limits-dialog/rate-limits-dialog.component.ts
@@ -2,6 +2,9 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ThemeService } from '../../services/theme.service';
 import { Observable } from 'rxjs';
+import { TorrentDataStoreService } from 'src/app/services/torrent-management/torrent-data-store.service';
+import { SnackbarService } from 'src/app/services/notifications/snackbar.service';
+import { UnitsHelperService } from 'src/app/services/units-helper.service';
 
 @Component({
   selector: 'app-rate-limits-dialog',
@@ -16,7 +19,11 @@ export class RateLimitsDialogComponent implements OnInit {
 
   public inputData: any;                   // Data passed in to this component
 
-  constructor(private theme: ThemeService, @Inject(MAT_DIALOG_DATA) inputData) { this.inputData = inputData }
+  constructor(private data_store: TorrentDataStoreService, private snackbar: SnackbarService, private units: UnitsHelperService,
+              private dialogRef: MatDialogRef<RateLimitsDialogComponent>, private theme: ThemeService, @Inject(MAT_DIALOG_DATA) inputData)
+              {
+                this.inputData = inputData
+              }
 
   ngOnInit(): void {
     this.isDarkTheme = this.theme.getThemeSubscription();
@@ -25,6 +32,22 @@ export class RateLimitsDialogComponent implements OnInit {
 
   async updateLimit() {
     this.isLoading = true;
+
+    switch (this.inputData.for) {
+      case 'Download':
+        await this.data_store.SetGlobalDownloadLimit(this.units.Kibibits_to_bits(this.newLimit));
+        break;
+
+      case 'Upload':
+        await this.data_store.SetGlobalUploadLimit(this.units.Kibibits_to_bits(this.newLimit));
+        break;
+
+      default:
+        this.snackbar.enqueueSnackBar(`Unknown limit update type ${this.inputData.for}!`, { type: 'error' });
+        break;
+    }
+
+    this.dialogRef.close();
   }
 
 }
